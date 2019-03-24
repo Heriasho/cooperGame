3730c186a70234741b5b2ea3dcb9e6d011ee3b88
Ryan Bartoli <ryanbartoli707@gmail.com>
2019-03-23 21:53:59 -0700

    Computer with MinMax added, but movement issues.

--- cooperGame.cpp
+++ cooperGame.cpp
@@ -1,9 +1,16 @@
+/*Ryan Bartoli
+3/23/19
+*/
 #include <iostream>
 #include <stdlib.h>
-#include <string> //For strings
-#include <map> //For hashmaps
+#include <array>
+#include <bits/stdc++.h> 
+#include <map>
+#include <cassert>
+#include <iterator> 
 #include <sstream>
-#include <cstring>
+#include <string>
+#include <time.h>
 using namespace std;
 
 /*Cooper Game:
@@ -14,23 +21,32 @@ Both player and computer generate their possible moves and displays them. Their 
 */
 const int ROW_SIZE = 5;
 const int COL_SIZE = 5;
-const int depth = 5;
+const int depthLimit = 50;
+int depth = 0;
+int maxDepthInit = 5;
+int maxDepth = maxDepthInit;
 const string LEFT = "LEFT";
 const string RIGHT = "RIGHT";
 const string TOP = "TOP";
 const string DOWN = "DOWN";
-char emptyCharacter = ' ';
+char emptyCharacter = '0';
+char wallCharacter = '+';
 char board[5][5];
-char storedPiece[depth][2];
+char storedPiece[depthLimit][2][2];
 bool debug = false;
+bool debugTimer = false;
+clock_t t;
 
 map<char,int> numbers;
 map<string,string> movement;
-string moveList[depth][200];
+string moveList[depthLimit][200];
 string boardString[5];
-int moveSize[depth];
+int moveSize[depthLimit];
 int playerPosition[2];
 int playerNewPosition[200][2];
+int computerPosition[2];
+int storedLocation[2];
+int alphabetaPruning[depthLimit][2];
 
 void setUp();
 void displayBoard();
@@ -38,25 +54,35 @@ void initBoardState();
 void humanMove();
 void computerMove();
 void hashMapThePieces();
+void movePiece(string selectedMove, bool isHuman);
+void undoMove(string selectedMove, bool isHuman);
 void moveGenerator(bool isHuman);
 void addToLegalMoveArray(int &move, string position);
 void printMoves();
+void minMax();
+int min();
+int max();
+int evaluate();
 
 bool isInBoard(int row, int col);
 bool isEmpty(char currentPiece);
 bool validateHumanMove(string move);
 bool escaped();
 bool capturedPlayer();
+bool endOfLevel();
 
 int main(){
     setUp();
     for(;;){
         humanMove();
-        if(escaped()){
+        displayBoard();
+        if(endOfLevel()){
             break;
         }
         computerMove();
-        if(capturedPlayer()){
+        displayBoard();
+        cout << "End of computer turn" << endl;
+        if(endOfLevel()){
             break;
         }
     }
@@ -107,41 +133,97 @@ void hashMapThePieces(){
     movement.insert(pair<const string,string>("DOWN","down"));
 
 }
-void movePiece(string selectedMove){
+void movePiece(string selectedMove,bool isHuman){
     int row,col;
-    row = playerPosition[1];
-    col = playerPosition[2];
-    storedPiece[depth][1] = board[row][col]; //Stored for later UNDO
+    int human = 1;
+    int computer = 2;
+    if(isHuman){
+        row = playerPosition[1];
+        col = playerPosition[2];
+        storedPiece[depth][1][human] = board[row][col]; //Stored for later UNDO
+    } else {
+        row = computerPosition[1];
+        col = computerPosition[2];
+        storedPiece[depth][1][computer] = board[row][col]; //Stored for later UNDO
+    }
     if(selectedMove == LEFT){
-        cout << "MOVE LEFT" << endl;
+        if(debug) cout << "MOVE LEFT" << endl;
+        if(isHuman) {
+            storedPiece[depth][2][human] = board[row][col-1]; //Stored for later UNDO
+        } else{
+            storedPiece[depth][2][computer] = board[row][col-1]; //Stored for later UNDO
+        }
         board[row][col-1] = board[row][col];	
         board[row][col] = emptyCharacter;
-        storedPiece[depth][2] = board[row][col-1]; //Stored for later UNDO
-        cout << "MOVING PIECE: " << row << col << " to " << row << col-1 << endl; 
+        storedLocation[1] = row;
+        storedLocation[2] = col-1;
+        if(debug) cout << "MOVING PIECE: " << row << col << " to " << row << col-1 << endl; 
+        if(debug) cout << "PIECES MOVED: " << storedPiece[depth][1] << " and " << storedPiece[depth][2] << endl;
     }
     if(selectedMove == TOP){
-        cout << "MOVE TOP" << endl;
+        if(debug) cout << "MOVE TOP" << endl;
+        if(isHuman){
+            storedPiece[depth][2][human] = board[row+1][col]; //Stored for later UNDO
+        } else {
+            storedPiece[depth][2][computer] = board[row+1][col]; //Stored for later UNDO
+        }
         board[row+1][col] = board[row][col];
         board[row][col] = emptyCharacter;
-        storedPiece[depth][2] = board[row+1][col]; //Stored for later UNDO
-        cout << "MOVING PIECE: " << row << col << " to " << row+1 << col << endl; 
+        storedLocation[1] = row+1;
+        storedLocation[2] = col;
+        if(debug) cout << "MOVING PIECE: " << row << col << " to " << row+1 << col << endl; 
+        if(debug) cout << "PIECES MOVED: " << storedPiece[depth][1] << " and " << storedPiece[depth][2] << endl;
     }
     if(selectedMove == RIGHT){
-        cout << "MOVE RIGHT" << endl;
+        if(debug) cout << "MOVE RIGHT" << endl;
+        if(isHuman){
+            storedPiece[depth][2][human] = board[row][col+1]; //Stored for later UNDO
+        } else {
+            storedPiece[depth][2][computer] = board[row][col+1]; //Stored for later UNDO
+        }
         board[row][col+1] = board[row][col];	
         board[row][col] = emptyCharacter;
-        storedPiece[depth][2] = board[row][col+1]; //Stored for later UNDO
-        cout << "MOVING PIECE: " << row << col << " to " << row << col+1 << endl; 
+        storedLocation[1] = row;
+        storedLocation[2] = col+1;
+        if(debug) cout << "MOVING PIECE: " << row << col << " to " << row << col+1 << endl; 
+        if(debug) cout << "PIECES MOVED: " << storedPiece[depth][1] << " and " << storedPiece[depth][2] << endl;
     }
     if(selectedMove == DOWN){
-        cout << "MOVE DOWN" << endl;
+        if(debug) cout << "MOVE DOWN" << endl;
+        if(isHuman){
+            storedPiece[depth][2][human] = board[row-1][col]; //Stored for later UNDO
+        } else {
+            storedPiece[depth][2][computer] = board[row-1][col]; //Stored for later UNDO
+        }
         board[row-1][col] = board[row][col];	
         board[row][col] = emptyCharacter;
-        storedPiece[depth][2] = board[row-1][col]; //Stored for later UNDO
-        cout << "MOVING PIECE: " << row << col << " to " << row-1 << col << endl; 
+        storedLocation[1] = row-1;
+        storedLocation[2] = col;
+        if(debug) cout << "MOVING PIECE: " << row << col << " to " << row-1 << col << endl; 
+        if(debug) cout << "PIECES MOVED: " << storedPiece[depth][1] << " and " << storedPiece[depth][2] << endl;
     }
 	//board[newCol][newRow] = board[col][row];	
 	//board[col][row] = emptyCharacter;
+}
+void undoMove(string selectedMove, bool isHuman){
+    int row,col,newRow,newCol,pivit;
+    char oldPiece;
+    newRow = storedLocation[1];
+    newCol = storedLocation[2];
+    if(isHuman){
+        pivit = 1;
+        row = playerPosition[1];
+        col = playerPosition[2];
+        oldPiece = storedPiece[depth][2][pivit];
+    } else {
+        pivit = 2;
+        row = computerPosition[1];
+        col = computerPosition[2];
+        oldPiece = storedPiece[depth][2][pivit];
+    }
+    if(debug) cout << "UNDO PIECE: " << row << col << " back to " << newRow << newCol << endl; 
+    board[row][col] = storedPiece[depth][1][pivit];
+    board[newRow][newCol] = storedPiece[depth][2][pivit];
 }
 void humanMove(){
     displayBoard();
@@ -168,7 +250,7 @@ void humanMove(){
         }
 
         if(validateHumanMove(input)){
-			movePiece(input);
+			movePiece(input, true);
 			break;
 		}
     }
@@ -176,6 +258,142 @@ void humanMove(){
 void computerMove(){
     cout << "Computer move" << endl;
     moveGenerator(false);
+    printMoves();
+    minMax();
+}
+void minMax(){
+	long timeRemaining = (long)CLOCKS_PER_SEC * 5.1;
+	if(debugTimer) cout << "Clicks allowed " << timeRemaining << endl;
+	long timeTaken = 0;
+	depth = 0;
+	string bestMove;
+	int bestScore;
+	int cnt = 1;
+	while(timeRemaining > timeTaken * 2 && maxDepth <= depthLimit){
+	//for(int cnt = 0; cnt < 1; cnt++){
+		t = clock();
+		if(debugTimer) cout << "Attempting depth " << maxDepth << " plys." << endl;
+		int thisScore = INT_MIN;
+		alphabetaPruning[depth][0] = INT_MIN; //Alpha lower bound
+		alphabetaPruning[depth][1] = INT_MAX; //Beta upper bound
+		string bestThis;
+		for(int i=0; i<moveSize[depth]; i++){ //LOOP THROUGH MOVES
+			string selectedMove = moveList[depth][i]; //Get the iterated move within moves at depth.
+			//cout << "Testing move " << selectedMove << endl;
+			movePiece(selectedMove, false);
+
+			alphabetaPruning[depth+1][0] = alphabetaPruning[depth][0];
+			alphabetaPruning[depth+1][1] = alphabetaPruning[depth][1];
+			int score = min();
+			//cout << "Eval depth " << depth << " is " << score << " for move " << selectedMove << endl;
+			//cout << "Score for move is " << score << endl;
+			if(score > thisScore){ 
+				//cout << "New best score " << score << endl;
+				bestThis = selectedMove;
+				thisScore=score;
+			}
+			if(alphabetaPruning[depth+1][0] > alphabetaPruning[depth][0]){
+				alphabetaPruning[depth][0] = alphabetaPruning[depth+1][0];
+			}
+			if(alphabetaPruning[depth][0] >= alphabetaPruning[depth][1]){
+				undoMove(selectedMove, false);
+				break;
+			}
+			undoMove(selectedMove, false);
+		}
+		t = clock() - t;
+		timeTaken = (long)t;
+		if(debugTimer) cout << "Clicks used " << timeTaken << ". Clicks remaining " << timeRemaining - timeTaken << endl;
+		timeRemaining -= timeTaken;
+		if(timeRemaining >= 0){
+			bestMove = bestThis;
+			bestScore = thisScore;
+		}
+		if(timeRemaining < 0){
+			cout << "Time ran out. Using result from " << maxDepth-1 << " plys." << endl;
+		}
+		maxDepth++;
+	}
+	maxDepth = maxDepthInit;
+	cout << "My move is " << bestMove << "  with a score of " << bestScore << endl;
+	movePiece(bestMove , true);
+}
+int min(){
+    depth++;
+    moveGenerator(true);
+    if(endOfLevel()){
+        depth--;
+        return (INT_MAX-depthLimit+depth);
+    }
+    if(depth == maxDepth){
+        int val = evaluate();
+        depth--;
+        return val;
+    }
+    int bestScore = INT_MAX;
+    string selectedMove;
+    for(int i=0;i<moveSize[depth];i++){
+        string selectedMove = moveList[depth][i];
+        movePiece(selectedMove, true);
+        alphabetaPruning[depth+1][0] = alphabetaPruning[depth][0];
+		alphabetaPruning[depth+1][1] = alphabetaPruning[depth][1];
+        int score = max();
+        if(score < bestScore){
+            bestScore = score;
+        }
+        if(alphabetaPruning[depth+1][0] < alphabetaPruning[depth][1]){
+			alphabetaPruning[depth][1] = alphabetaPruning[depth+1][0];
+		}
+		if(alphabetaPruning[depth][0] >= alphabetaPruning[depth][1]){
+			undoMove(selectedMove, true);
+			break;
+		}
+        undoMove(selectedMove, true);
+    }
+    depth--;
+    return(bestScore);
+}
+int max(){
+    depth++;
+    moveGenerator(false);
+    if(endOfLevel()){
+        depth--;
+        return (INT_MIN+depthLimit-depth);
+    }
+    if(depth == maxDepth){
+        int val = evaluate();
+        depth--;
+        return val;
+    }
+    int bestScore = INT_MIN;
+    string selectedMove;
+    for(int i=0; i<moveSize[depth];i++){
+        string selectedMove = moveList[depth][i];
+        movePiece(selectedMove, false);
+        alphabetaPruning[depth+1][0] = alphabetaPruning[depth][0];
+		alphabetaPruning[depth+1][1] = alphabetaPruning[depth][1];
+        int score = min();
+        if(score > bestScore){
+            bestScore = score;
+        }
+        if(alphabetaPruning[depth+1][1] > alphabetaPruning[depth][0]){
+			alphabetaPruning[depth][0] = alphabetaPruning[depth+1][1];
+		}
+		if(alphabetaPruning[depth][0] >= alphabetaPruning[depth][1]){
+			undoMove(selectedMove, false);
+			break;
+		}
+        undoMove(selectedMove, false);
+    }
+    depth--;
+    return(bestScore);
+}
+int evaluate(){
+    int i = 0;
+    if(capturedPlayer){
+        i += 5000;
+    }
+    return i;
 }
 void moveGenerator(bool isHuman){
     int move = 0;
@@ -215,7 +433,9 @@ void moveGenerator(bool isHuman){
                 }
                 break;
             } else if(!isHuman && currentPiece == '!'){
-                cout << "Computer is located at " << row << col << endl;
+                if(debug) cout << "Computer is located at " << row << col << endl;
+                computerPosition[1] = row;
+                computerPosition[2] = col;
                 if(isEmpty(LEFT_POS) && isInBoard(row,col-1)){
                     addToLegalMoveArray(move, LEFT);
                 }
@@ -263,7 +483,7 @@ bool validateHumanMove(string move){
 	return false;
 }
 bool isEmpty(char currentPiece){
-    return (currentPiece == emptyCharacter);
+    return (currentPiece == emptyCharacter || currentPiece == wallCharacter);
 }
 bool isInBoard(int row, int col){ //Lower_row = -1, Upper_row = 7, Lower_col = -1, Upper_col = 8
     //if(row > -1 && col > -1) cout << "Move " << row << col << " > -1" << endl;
@@ -277,5 +497,20 @@ bool escaped(){
     return false;
 }
 bool capturedPlayer(){
+	for(int i=0;i<ROW_SIZE;i++){
+		for(int j=0;j<COL_SIZE;j++){
+			if(board[i][j] == '&'){
+                if(debug) cout << "Player still alive at depth " << depth << endl;
+				return false;
+            }
+        }
+    }
+    return true;
+}
+bool endOfLevel(){
+    if(capturedPlayer()){
+        if(debug) if(depth) cout << "Caught player at depth " << depth << endl;
+        return true;
+    }
     return false;
 }
